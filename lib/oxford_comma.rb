7aa33feb9ea55a21a41c4c80001961719817c162
Julie Evans <juliemevans17@gmail.com>
2022-02-19 19:55:59 -0600

    commit tree 4f9920712c741da99ba44639718e8ed27f2e43e4

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  else
+    last = "and " << array[-1]
+    array.pop
+    array << last 
+    if array.length == 2
+        array.join(" ")
+    else
+      array.join(", ")
+    end
+  end
 end
